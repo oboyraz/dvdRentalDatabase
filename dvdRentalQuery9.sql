357608b6b9a79a874664e17bb873790b03b89cf1
omerboyraz <omer.boyraz@isuzu.com.tr>
2022-12-19 16:40:18 +0300

    JOIN

---
+++ dvdRentalQuery9.sql
@@ -0,0 +1,21 @@
+
+
+-- Write the LEFT JOIN query where we can see the city and country names in the city table and the country table together.
+
+SELECT city, country FROM city
+LEFT JOIN country ON city.country_id = country.country_id;
+
+
+-- Write the RIGHT JOIN query where we can see the customer table and the payment_id in the payment table and the first_name and last_name names in the customer table together.
+
+SELECT payment_id, first_name, last_name FROM payment
+RIGHT JOIN customer ON payment.customer_id = customer.customer_id;
+
+
+-- Write the FULL JOIN query where we can see the customer table and the rental_id in the rental table and the first_name and last_name names in the customer table together.
+
+SELECT rental_id, first_name, last_name FROM customer
+FULL JOIN rental ON customer.customer_id = rental.customer_id;
+
+
+
